cf138aebc7ac228bf06b61bbc6371380a9f118bd
Jamil Zazu <jamillzazu@gmail.com>
2021-05-01 14:41:12 -0400

    feat: Utilizando  Context API no React

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Dashboard } from "./componentes/Dashboard ";
 import { Header } from "./componentes/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from './componentes/NewTransactionModal';
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -20,13 +21,13 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransctionModal} />
       <Dashboard />
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransctionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
