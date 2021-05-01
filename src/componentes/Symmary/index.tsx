7e6229cf9ba631fcf89c5bb247bb8d67857b8058
Jamil Zazu <jamillzazu@gmail.com>
2021-05-01 15:38:08 -0400

    feat: Calculando resumo

--- src/componentes/Symmary/index.tsx
+++ src/componentes/Symmary/index.tsx
@@ -9,6 +9,22 @@ import { Container } from "./styles";
 export function Symmary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    }
+    else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+    }
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
+
   return (
     <Container>
       <div>
@@ -16,7 +32,15 @@ export function Symmary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {
+            new Intl.NumberFormat('pt-BR',
+              {
+                style: 'currency',
+                currency: 'BRL'
+              }).format(summary.deposits)
+          }
+        </strong>
       </div>
 
       <div>
@@ -24,7 +48,13 @@ export function Symmary() {
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>- {
+          new Intl.NumberFormat('pt-BR',
+            {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.withdraws)
+        }</strong>
       </div>
 
       <div className="highlight-backgroud">
@@ -32,7 +62,15 @@ export function Symmary() {
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {
+            new Intl.NumberFormat('pt-BR',
+              {
+                style: 'currency',
+                currency: 'BRL'
+              }).format(summary.total)
+          }
+        </strong>
       </div>
     </Container>
   )
